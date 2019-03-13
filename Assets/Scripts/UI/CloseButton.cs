825298a58f324061e0638ead70e981f66ccbc5c9
PROJ PC27 rafael <rafaelrbenavent@gmail.com>
2019-03-13 15:04:25 +1100

    ui changes

---
+++ Assets/Scripts/UI/CloseButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MeccanoMechanic
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Button))]
+    [AddComponentMenu("Meccano Mechanic/UI/CloseButton")]
+    public sealed class CloseButton : MonoBehaviour
+    {
+        // object to destroy when this button is clicked
+        [SerializeField] private GameObject target;
+
+        private void Start()
+        {
+            this.GetComponent<Button>().onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnButtonClick()
+        {
+            Destroy(target);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DuplicateButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MeccanoMechanic
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Button))]
+    [AddComponentMenu("Meccano Mechanic/UI/DuplicateButton")]
+    public sealed class DuplicateButton : MonoBehaviour
+    {
+        // object to duplicate when this button is clicked
+        [SerializeField] private GameObject target;
+
+        // position offset when duplicating
+        [SerializeField] private Vector3 positionOffset;
+
+        private void Start()
+        {
+            this.GetComponent<Button>().onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnButtonClick()
+        {
+            var rotation = target.transform.rotation;
+            var position = new Vector3(target.transform.position.x + positionOffset.x,
+                target.transform.localPosition.y + positionOffset.y,
+                target.transform.localPosition.z + positionOffset.z);
+            var clone = Instantiate(target, position, rotation);
+            clone.transform.SetParent(target.transform.parent, false);
+        }
+    }
+}
